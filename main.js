28beabd9369941aaa790899ea77683226c4adf1a
Avram Eisner <avrameisner@hazel.co>
2024-04-24 15:41:50 -0700

    drawing random rectangles

--- main.js
+++ main.js
@@ -1,109 +1,154 @@
-import './style.css'
+import "./style.css";
 
-const vertexShaderURL = '/shaders/basic.vert'
-const fragmentShaderURL = '/shaders/basic.frag'
-
-const shadersResponses = await Promise.all([fetch(vertexShaderURL), fetch(fragmentShaderURL)])
-const [vertexShaderSource, fragmentShaderSource] = await Promise.all(shadersResponses.map(sr => sr.text()))
+const vertexShaderURL = "/shaders/basic.vert";
+const fragmentShaderURL = "/shaders/basic.frag";
 
 function createShader(gl, type, source) {
-  const shader = gl.createShader(type)
-  gl.shaderSource(shader, source)
-  gl.compileShader(shader)
-  const success = gl.getShaderParameter(shader, gl.COMPILE_STATUS)
+  const shader = gl.createShader(type);
+  gl.shaderSource(shader, source);
+  gl.compileShader(shader);
+  const success = gl.getShaderParameter(shader, gl.COMPILE_STATUS);
   if (success) {
-    return shader
+    return shader;
   }
 
-  console.log(gl.getShaderInfoLog(shader))  // eslint-disable-line
-  gl.deleteShader(shader)
-  return undefined
+  console.log(gl.getShaderInfoLog(shader)); // eslint-disable-line
+  gl.deleteShader(shader);
+  return undefined;
 }
 
 function createProgram(gl, vertexShader, fragmentShader) {
-  const program = gl.createProgram()
-  gl.attachShader(program, vertexShader)
-  gl.attachShader(program, fragmentShader)
-  gl.linkProgram(program)
-  const success = gl.getProgramParameter(program, gl.LINK_STATUS)
+  const program = gl.createProgram();
+  gl.attachShader(program, vertexShader);
+  gl.attachShader(program, fragmentShader);
+  gl.linkProgram(program);
+  const success = gl.getProgramParameter(program, gl.LINK_STATUS);
   if (success) {
-    return program
+    return program;
   }
 
-  console.log(gl.getProgramInfoLog(program))  // eslint-disable-line
-  gl.deleteProgram(program)
-  return undefined
+  console.log(gl.getProgramInfoLog(program)); // eslint-disable-line
+  gl.deleteProgram(program);
+  return undefined;
 }
 
-function main() {
+// Returns a random integer from 0 to range - 1.
+function randomInt(range) {
+  return Math.floor(Math.random() * range);
+}
+
+// Fills the buffer with the values that define a rectangle.
+
+function setRectangle(gl, x, y, width, height) {
+  const x1 = x;
+  const x2 = x + width;
+  const y1 = y;
+  const y2 = y + height;
+
+  // NOTE: gl.bufferData(gl.ARRAY_BUFFER, ...) will affect
+  // whatever buffer is bound to the `ARRAY_BUFFER` bind point
+  // but so far we only have one buffer. If we had more than one
+  // buffer we'd want to bind that buffer to `ARRAY_BUFFER` first.
+
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array([
+    x1, y1,
+    x2, y1,
+    x1, y2,
+    x1, y2,
+    x2, y1,
+    x2, y2]), gl.STATIC_DRAW);
+}
+
+async function main() {
+  // Load the shader files
+  const shadersResponses = await Promise.all([
+    fetch(vertexShaderURL),
+    fetch(fragmentShaderURL),
+  ]);
+  const [vertexShaderSource, fragmentShaderSource] = await Promise.all(
+    shadersResponses.map((sr) => sr.text())
+  );
+
   // Get A WebGL context
   const canvas = document.querySelector("#canvas");
   const gl = canvas.getContext("webgl2");
   if (!gl) {
-    return
+    return;
   }
 
   // create GLSL shaders, upload the GLSL source, compile the shaders
-  const vertexShader = createShader(gl, gl.VERTEX_SHADER, vertexShaderSource)
-  const fragmentShader = createShader(gl, gl.FRAGMENT_SHADER, fragmentShaderSource)
+  const vertexShader = createShader(gl, gl.VERTEX_SHADER, vertexShaderSource);
+  const fragmentShader = createShader(gl, gl.FRAGMENT_SHADER, fragmentShaderSource);
 
   // Link the two shaders into a program
-  const program = createProgram(gl, vertexShader, fragmentShader)
+  const program = createProgram(gl, vertexShader, fragmentShader);
 
   // look up where the vertex data needs to go.
-  const positionAttributeLocation = gl.getAttribLocation(program, "a_position")
+  const positionAttributeLocation = gl.getAttribLocation(program, "a_position");
+  const resolutionUniformLocation = gl.getUniformLocation(program, "u_resolution");
+  const colorLocation = gl.getUniformLocation(program, "u_color");
 
   // Create a buffer and put three 2d clip space points in it
-  const positionBuffer = gl.createBuffer()
+  const positionBuffer = gl.createBuffer();
 
   // Bind it to ARRAY_BUFFER (think of it as ARRAY_BUFFER = positionBuffer)
-  gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer)
-
-  var positions = [
-    0, 0,
-    0, 0.5,
-    0.7, 0,
-  ]
-  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW)
+  gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
 
   // Create a vertex array object (attribute state)
-  const vao = gl.createVertexArray()
+  const vao = gl.createVertexArray();
 
   // and make it the one we're currently working with
-  gl.bindVertexArray(vao)
+  gl.bindVertexArray(vao);
 
   // Turn on the attribute
-  gl.enableVertexAttribArray(positionAttributeLocation)
+  gl.enableVertexAttribArray(positionAttributeLocation);
 
   // Tell the attribute how to get data out of positionBuffer (ARRAY_BUFFER)
-  const size = 2          // 2 components per iteration
-  const type = gl.FLOAT   // the data is 32bit floats
-  const normalize = false // don't normalize the data
-  const stride = 0        // 0 = move forward size * sizeof(type) each iteration to get the next position
-  const offset_1 = 0      // start at the beginning of the buffer
-  gl.vertexAttribPointer(positionAttributeLocation, size, type, normalize, stride, offset_1)
+  const size = 2; // 2 components per iteration
+  const type = gl.FLOAT; // the data is 32bit floats
+  const normalize = false; // don't normalize the data
+  const stride = 0; // 0 = move forward size * sizeof(type) each iteration to get the next position
+  let offset = 0; // start at the beginning of the buffer
+  gl.vertexAttribPointer(
+    positionAttributeLocation,
+    size,
+    type,
+    normalize,
+    stride,
+    offset
+  );
 
-  webglUtils.resizeCanvasToDisplaySize(gl.canvas)
+  webglUtils.resizeCanvasToDisplaySize(gl.canvas);
 
   // Tell WebGL how to convert from clip space to pixels
-  gl.viewport(0, 0, gl.canvas.width, gl.canvas.height)
+  gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
 
   // Clear the canvas
-  gl.clearColor(0, 0, 0, 0)
-  gl.clear(gl.COLOR_BUFFER_BIT)
+  gl.clearColor(0, 0, 0, 0);
+  gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
   // Tell it to use our program (pair of shaders)
-  gl.useProgram(program)
+  gl.useProgram(program);
+
+  gl.uniform2f(resolutionUniformLocation, gl.canvas.width, gl.canvas.height);
 
   // Bind the attribute/buffer set we want.
-  gl.bindVertexArray(vao)
+  gl.bindVertexArray(vao);
 
-  // draw
-  const primitiveType = gl.TRIANGLES
-  const offset_2 = 0
-  const count = 3
-  gl.drawArrays(primitiveType, offset_2, count);
+  // draw 50 random rectangles in random colors
+  for (let ii = 0; ii < 50; ++ii) {
+    // Setup a random rectangle
+    setRectangle(gl, randomInt(gl.canvas.width), randomInt(gl.canvas.height), randomInt(300), randomInt(300));
+
+    // Set a random color.
+    gl.uniform4f(colorLocation, Math.random(), Math.random(), Math.random(), 1);
+
+    // Draw the rectangle.
+    const primitiveType = gl.TRIANGLES;
+    const offset = 0;
+    const count = 6;
+    gl.drawArrays(primitiveType, offset, count);
+  }
 }
 
-main()
-
+main();
