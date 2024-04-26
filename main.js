0d616c8db7e3b5710843f6be3e632681e692e1b5
Avram Eisner <avrame@gmail.com>
2024-04-26 15:32:34 -0700

    Updated to use better coloration

--- main.js
+++ main.js
@@ -1,154 +1,98 @@
-import "./style.css";
+/* locate the canvas element */
+const canvas_element = document.getElementById("maincanvas");
 
-const vertexShaderURL = "/shaders/basic.vert";
-const fragmentShaderURL = "/shaders/basic.frag";
+/* obtain a webgl rendering context */
+const gl = canvas_element.getContext("webgl");
 
-function createShader(gl, type, source) {
-  const shader = gl.createShader(type);
-  gl.shaderSource(shader, source);
-  gl.compileShader(shader);
-  const success = gl.getShaderParameter(shader, gl.COMPILE_STATUS);
-  if (success) {
-    return shader;
-  }
+/* get shader code from the <script> tags */
+const vertex_shader_src = document.getElementById("shader-vs").text;
+const fragment_shader_src = document.getElementById("shader-fs").text;
 
-  console.log(gl.getShaderInfoLog(shader)); // eslint-disable-line
-  gl.deleteShader(shader);
-  return undefined;
-}
+// Use our boilerplate utils to compile the shaders and link into a program
+const mandelbrot_program = webglUtils.createProgramFromSources(gl, [
+  vertex_shader_src,
+  fragment_shader_src,
+]);
+// Tell it to use our program (pair of shaders)
+gl.useProgram(mandelbrot_program);
 
-function createProgram(gl, vertexShader, fragmentShader) {
-  const program = gl.createProgram();
-  gl.attachShader(program, vertexShader);
-  gl.attachShader(program, fragmentShader);
-  gl.linkProgram(program);
-  const success = gl.getProgramParameter(program, gl.LINK_STATUS);
-  if (success) {
-    return program;
-  }
+/* create a vertex buffer for a full-screen triangle */
+const vertex_buf = gl.createBuffer(gl.ARRAY_BUFFER);
+gl.bindBuffer(gl.ARRAY_BUFFER, vertex_buf);
+gl.bufferData(
+  gl.ARRAY_BUFFER,
+  new Float32Array([-1, -1, 3, -1, -1, 3]),
+  gl.STATIC_DRAW
+);
 
-  console.log(gl.getProgramInfoLog(program)); // eslint-disable-line
-  gl.deleteProgram(program);
-  return undefined;
-}
+/* set up the position attribute */
+const position_attrib_location = gl.getAttribLocation(
+  mandelbrot_program,
+  "a_Position"
+);
+gl.enableVertexAttribArray(position_attrib_location);
+gl.vertexAttribPointer(position_attrib_location, 2, gl.FLOAT, false, 0, 0);
 
-// Returns a random integer from 0 to range - 1.
-function randomInt(range) {
-  return Math.floor(Math.random() * range);
-}
+/* find uniform locations */
+const zoom_center_uniform = gl.getUniformLocation(
+  mandelbrot_program,
+  "u_zoomCenter"
+);
+const zoom_size_uniform = gl.getUniformLocation(mandelbrot_program, "u_zoomSize");
+const max_iterations_uniform = gl.getUniformLocation(
+  mandelbrot_program,
+  "u_maxIterations"
+);
 
-// Fills the buffer with the values that define a rectangle.
+/* these hold the state of zoom operation */
+const zoom_center = [0.0, 0.0];
+let target_zoom_center = [0.0, 0.0];
+let zoom_size = 4.0;
+let stop_zooming = true;
+let zoom_factor = 1.0;
+const max_iterations = 500;
 
-function setRectangle(gl, x, y, width, height) {
-  const x1 = x;
-  const x2 = x + width;
-  const y1 = y;
-  const y2 = y + height;
+function renderFrame() {
+  /* bind inputs & render frame */
+  gl.uniform2f(zoom_center_uniform, zoom_center[0], zoom_center[1]);
+  gl.uniform1f(zoom_size_uniform, zoom_size);
+  gl.uniform1i(max_iterations_uniform, max_iterations);
+  gl.clearColor(0.0, 0.0, 0.0, 1.0);
+  gl.clear(gl.COLOR_BUFFER_BIT);
+  gl.drawArrays(gl.TRIANGLES, 0, 3);
 
-  // NOTE: gl.bufferData(gl.ARRAY_BUFFER, ...) will affect
-  // whatever buffer is bound to the `ARRAY_BUFFER` bind point
-  // but so far we only have one buffer. If we had more than one
-  // buffer we'd want to bind that buffer to `ARRAY_BUFFER` first.
+  /* handle zoom */
+  if (!stop_zooming) {
+    /* zoom in */
+    zoom_size *= zoom_factor;
 
-  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array([
-    x1, y1,
-    x2, y1,
-    x1, y2,
-    x1, y2,
-    x2, y1,
-    x2, y2]), gl.STATIC_DRAW);
-}
+    /* move zoom center towards target */
+    zoom_center[0] += 0.1 * (target_zoom_center[0] - zoom_center[0]);
+    zoom_center[1] += 0.1 * (target_zoom_center[1] - zoom_center[1]);
 
-async function main() {
-  // Load the shader files
-  const shadersResponses = await Promise.all([
-    fetch(vertexShaderURL),
-    fetch(fragmentShaderURL),
-  ]);
-  const [vertexShaderSource, fragmentShaderSource] = await Promise.all(
-    shadersResponses.map((sr) => sr.text())
-  );
-
-  // Get A WebGL context
-  const canvas = document.querySelector("#canvas");
-  const gl = canvas.getContext("webgl2");
-  if (!gl) {
-    return;
-  }
-
-  // create GLSL shaders, upload the GLSL source, compile the shaders
-  const vertexShader = createShader(gl, gl.VERTEX_SHADER, vertexShaderSource);
-  const fragmentShader = createShader(gl, gl.FRAGMENT_SHADER, fragmentShaderSource);
-
-  // Link the two shaders into a program
-  const program = createProgram(gl, vertexShader, fragmentShader);
-
-  // look up where the vertex data needs to go.
-  const positionAttributeLocation = gl.getAttribLocation(program, "a_position");
-  const resolutionUniformLocation = gl.getUniformLocation(program, "u_resolution");
-  const colorLocation = gl.getUniformLocation(program, "u_color");
-
-  // Create a buffer and put three 2d clip space points in it
-  const positionBuffer = gl.createBuffer();
-
-  // Bind it to ARRAY_BUFFER (think of it as ARRAY_BUFFER = positionBuffer)
-  gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
-
-  // Create a vertex array object (attribute state)
-  const vao = gl.createVertexArray();
-
-  // and make it the one we're currently working with
-  gl.bindVertexArray(vao);
-
-  // Turn on the attribute
-  gl.enableVertexAttribArray(positionAttributeLocation);
-
-  // Tell the attribute how to get data out of positionBuffer (ARRAY_BUFFER)
-  const size = 2; // 2 components per iteration
-  const type = gl.FLOAT; // the data is 32bit floats
-  const normalize = false; // don't normalize the data
-  const stride = 0; // 0 = move forward size * sizeof(type) each iteration to get the next position
-  let offset = 0; // start at the beginning of the buffer
-  gl.vertexAttribPointer(
-    positionAttributeLocation,
-    size,
-    type,
-    normalize,
-    stride,
-    offset
-  );
-
-  webglUtils.resizeCanvasToDisplaySize(gl.canvas);
-
-  // Tell WebGL how to convert from clip space to pixels
-  gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
-
-  // Clear the canvas
-  gl.clearColor(0, 0, 0, 0);
-  gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-
-  // Tell it to use our program (pair of shaders)
-  gl.useProgram(program);
-
-  gl.uniform2f(resolutionUniformLocation, gl.canvas.width, gl.canvas.height);
-
-  // Bind the attribute/buffer set we want.
-  gl.bindVertexArray(vao);
-
-  // draw 50 random rectangles in random colors
-  for (let ii = 0; ii < 50; ++ii) {
-    // Setup a random rectangle
-    setRectangle(gl, randomInt(gl.canvas.width), randomInt(gl.canvas.height), randomInt(300), randomInt(300));
-
-    // Set a random color.
-    gl.uniform4f(colorLocation, Math.random(), Math.random(), Math.random(), 1);
-
-    // Draw the rectangle.
-    const primitiveType = gl.TRIANGLES;
-    const offset = 0;
-    const count = 6;
-    gl.drawArrays(primitiveType, offset, count);
+    window.requestAnimationFrame(renderFrame);
+  } else if (max_iterations < 500) {
+    window.requestAnimationFrame(renderFrame);
   }
 }
 
-main();
+/* input handling */
+canvas_element.onmousedown = function (e) {
+  const x_part = e.offsetX / canvas_element.width;
+  const y_part = e.offsetY / canvas_element.height;
+  target_zoom_center[0] = zoom_center[0] - zoom_size / 2.0 + x_part * zoom_size;
+  target_zoom_center[1] = zoom_center[1] + zoom_size / 2.0 - y_part * zoom_size;
+  stop_zooming = false;
+  zoom_factor = e.buttons & 1 ? 0.99 : 1.01;
+  renderFrame();
+  return true;
+};
+canvas_element.oncontextmenu = function (e) {
+  return false;
+};
+canvas_element.onmouseup = function (e) {
+  stop_zooming = true;
+};
+
+/* display initial frame */
+renderFrame();
--- public/shaders/basic.frag
+++ public/shaders/basic.frag
@@ -4,12 +4,34 @@
 // to pick one. highp is a good default. It means "high precision"
 precision highp float;
 
-uniform vec4 u_color;
+uniform vec2 u_zoomCenter;
+uniform float u_zoomSize;
+uniform int u_maxIterations;
 
 // we need to declare an output for the fragment shader
 out vec4 outColor;
 
+vec2 f(vec2 x, vec2 c) {
+  return mat2(x, -x.y, x.x) * x + c;
+}
+vec3 palette(float t, vec3 a, vec3 b, vec3 c, vec3 d) {
+  return a + b * cos(6.28318f * (c * t + d));
+}
 void main() {
-  // Just set the output to a constant redish-purple
-  outColor = u_color;
+  vec2 uv = gl_FragCoord.xy / vec2(800.0f, 800.0f);
+  vec2 c = u_zoomCenter + (uv * 4.0f - vec2(2.0f)) * (u_zoomSize / 4.0f);
+  vec2 x = vec2(0.0f);
+  bool escaped = false;
+  int iterations = 0;
+  for(int i = 0; i < 10000; i++) {
+    if(i > u_maxIterations)
+      break;
+    iterations = i;
+    x = f(x, c);
+    if(length(x) > 2.0f) {
+      escaped = true;
+      break;
+    }
+  }
+  outColor = escaped ? vec4(palette(float(iterations) / float(u_maxIterations), vec3(0.0f), vec3(0.59f, 0.55f, 0.75f), vec3(0.1f, 0.2f, 0.3f), vec3(0.75f)), 1.0f) : vec4(vec3(0.85f, 0.99f, 1.0f), 1.0f);
 }
